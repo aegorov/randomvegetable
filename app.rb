812420d82cfb562bb3fe024059ffc1e594235b7c
Artur Egorov <artur@Artur-Egorovs-iMac.local>
2011-07-18 12:59:53 +0400

    translate with ms-translator gem

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 # encoding: utf-8
+require 'net/http'
+require 'nokogiri'
 require 'sinatra'
 require 'erb'
+require 'ms-translator'
 
+Microsoft::Translator.set_app_id('DC0080E42D4D34850E763304034C6767D39C80C0')
 
 get "/" do
-  vegetables = ["broccoli",'cucumber','tomato']
-  @vegetables_with_translate = {"broccoli" => "броколи", "cucumber" =>"огурец", "tomato" => "томат"}
+  vegetables = ['broccoli','cucumber','tomato','Garlic','Lentil','Earthnut pea',
+                'Carrot','Wasabi','Hijiki','Beetroot','Common bean','Bamboo shoot',
+                'Seakale','Indian pea','Celery','Ogonori','Gobo','Sweet pepper',
+                'Pumpkin','Cress','Gim','Tepary bean','Ti','Yam','Samphire',
+                'Mung bean','Wakame','Spinach','Chard','Yarrow','Pigeon pea']
   @random_vegetable = vegetables.at(Random.rand(vegetables.size))
+  @translate = Microsoft::Translator("#{@random_vegetable}", 'en', 'ru')
   erb :index
 end
