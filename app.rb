218a17629ee8b92e8a981712c8fcccadc48a8268
Artur Egorov <artur@Artur-Egorovs-iMac.local>
2011-07-18 13:16:23 +0400

    add vegetables

--- app.rb
+++ app.rb
@@ -8,11 +8,13 @@ require 'ms-translator'
 Microsoft::Translator.set_app_id('DC0080E42D4D34850E763304034C6767D39C80C0')
 
 get "/" do
-  vegetables = ['broccoli','cucumber','tomato','Garlic','Lentil','Earthnut pea',
-                'Carrot','Wasabi','Hijiki','Beetroot','Common bean','Bamboo shoot',
-                'Seakale','Indian pea','Celery','Ogonori','Gobo','Sweet pepper',
-                'Pumpkin','Cress','Gim','Tepary bean','Ti','Yam','Samphire',
-                'Mung bean','Wakame','Spinach','Chard','Yarrow','Pigeon pea']
+  vegetables = ['Broccoli','Cucumber','Tomato','Cabbage','Kohlrabi',
+                'Chard','Parsley','Dill','Basil','Savory','Mint',
+                'Melissa','Rumex','Spinach','Fennel','Pepper','Eggplant',
+                'Courgette','Pattypan Squash','Pumpkin','Peas','Beans',
+                'Potatoes','Carrots','Beet','Radish','Radishes','Horseradish',
+                'Celery','Cassava','Jerusalem Artichoke','Turnip','Rutabaga',
+                'Burdock','Arracacha','Onion','Garlic','Asparagus','Rhubarb']
   @random_vegetable = vegetables.at(Random.rand(vegetables.size))
   @translate = Microsoft::Translator("#{@random_vegetable}", 'en', 'ru')
   erb :index
