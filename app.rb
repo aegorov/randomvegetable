7a7a952f047e1ffdae0464ec7e994bde643ee1b3
Artur Egorov <artur@egorov.in>
2013-02-03 15:16:21 +0400

    hide api key

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ require 'sinatra'
 require 'erb'
 require 'ms-translator'
 
-Microsoft::Translator.set_app_id('DC0080E42D4D34850E763304034C6767D39C80C0')
+Microsoft::Translator.set_app_id(ENV['API_KEY'])
 
 get "/" do
-  vegetables = ['Broccoli','Cucumber','Tomato','Cabbage','Kohlrabi',
-                'Chard','Parsley','Dill','Basil','Savory','Mint',
-                'Melissa','Rumex','Spinach','Fennel','Pepper','Eggplant',
-                'Courgette','Pattypan Squash','Pumpkin','Peas','Beans',
-                'Potatoes','Carrots','Beet','Radish','Radishes','Horseradish',
-                'Celery','Cassava','Jerusalem Artichoke','Turnip','Rutabaga',
-                'Burdock','Arracacha','Onion','Garlic','Asparagus','Rhubarb']
+  vegetables = %w(Broccoli Cucumber Tomato Cabbage Kohlrabi
+                Chard Parsley Dill Basil Savory Mint
+                Melissa Rumex Spinach Fennel Pepper Eggplant
+                Courgette Pattypan Squash Pumpkin, Peas, Beans,
+                Potatoes Carrots Beet Radish Radishes Horseradish
+                Celery Cassava Jerusalem Artichoke Turnip Rutabaga
+                Burdock Arracacha Onion Garlic Asparagus Rhubarb)
   @random_vegetable = vegetables.sample
   @translate = Microsoft::Translator(@random_vegetable, 'en', 'ru')
   erb :index
