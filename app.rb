b7f5fd17f66ce10e8eb9f23d197abc5bb1a4950b
Artur Egorov <artur@egorov.in>
2013-02-04 15:10:26 +0400

    use yandex-translator instead of ms-translator

--- app.rb
+++ app.rb
@@ -4,19 +4,18 @@ require 'net/http'
 require 'nokogiri'
 require 'sinatra'
 require 'erb'
-require 'ms-translator'
+require 'yandex-translator'
 
-Microsoft::Translator.set_app_id(ENV['API_KEY'])
 
 get "/" do
   vegetables = %w(Broccoli Cucumber Tomato Cabbage Kohlrabi
                 Chard Parsley Dill Basil Savory Mint
                 Melissa Rumex Spinach Fennel Pepper Eggplant
-                Courgette Pattypan Squash Pumpkin, Peas, Beans,
+                Courgette Pattypan Squash Pumpkin Peas Beans
                 Potatoes Carrots Beet Radish Radishes Horseradish
                 Celery Cassava Jerusalem Artichoke Turnip Rutabaga
                 Burdock Arracacha Onion Garlic Asparagus Rhubarb)
   @random_vegetable = vegetables.sample
-  @translate = Microsoft::Translator(@random_vegetable, 'en', 'ru')
+  @translate = Yandex::Translator.translate(@random_vegetable, 'ru')
   erb :index
 end
